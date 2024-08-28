457c11c7c4da5cb1d784e54a498e8eeac3ebefa1
DeePAk05082002 <05082002.chaudhary.deepak@gmail.com>
2024-08-28 11:43:11 +0530

    2nd approach

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core' ;
 
 @Component({
     selector: 'app-header',
-    standalone: true,
+    standalone: false,
     styleUrl: './header.component.css',
     templateUrl: './header.component.html'
 })
